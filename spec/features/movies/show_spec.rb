6953df320fcddd8002fd3137beed6e75176c096b
Sam Devine <samuel.will.devine@gmail.com>
2021-11-02 10:19:58 -0600

    test: add-actor form

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'the movie show page' do
   describe 'as a user' do
     before :each do
       @studio = Studio.create!(name: 'Walt Disney Studios', location: 'Burbank, CA')
-      @movie = @studio.movies.create!(title: 'Pirates of the Caribbean: The Curse of the Black Pearl', creation_year: 2003, genre: 'Fantasy')
+      @movie = @studio.movies.create(title: 'Pirates of the Caribbean: The Curse of the Black Pearl', creation_year: 2003, genre: 'Fantasy')
 
-      @actor_1 = @movie.actors.create!(name: 'Johnny Depp', age: 58)
-      @actor_2 = @movie.actors.create!(name: 'Keira Knightley', age: 36)
-      @actor_3 = @movie.actors.create!(name: 'Orlando Bloom', age: 44)
+      @actor_1 = @movie.actors.create(name: 'Johnny Depp', age: 58)
+      @actor_2 = @movie.actors.create(name: 'Keira Knightley', age: 36)
+      @actor_3 = @movie.actors.create(name: 'Orlando Bloom', age: 44)
     end
 
     it 'I see the movies title, creation year, and genre' do
@@ -31,6 +31,34 @@ RSpec.describe 'the movie show page' do
       visit movie_path(@movie)
 
       expect(page).to have_content 'Average Age of Actors: 46.0'
+    end
+
+    it 'I do not see any actors that are not part of the movie' do
+      actor_4 = Actor.create(name: 'George Clooney', age: 60)
+
+      visit movie_path(@movie)
+
+      expect(page).to_not have_content actor_4.name
+    end
+
+    it 'I see a form to add an actor to this movie' do
+      visit movie_path(@movie)
+
+      within "div#add-actor" do
+        expect(page).to have_content 'Add an actor to this movie:'
+        expect(page).to have_button 'Submit'
+      end
+    end
+
+    it 'the form adds existing actors to the movie' do
+      actor_4 = Actor.create(name: 'George Clooney', age: 60)
+
+      visit movie_path(@movie)
+      fill_in 'Name', with: actor_4.name
+      click_on 'Submit'
+
+      expect(current_path).to eq movie_path(@movie)
+      expect(page).to have_content actor_4.name
     end
   end
 end
--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'the studio index page' do
   describe 'as a user' do
     it 'I see each studios name and location' do
-      studio_1 = Studio.create!(name: 'Universal Pictures', location: 'Universal City, CA')
-      studio_2 = Studio.create!(name: 'Walt Disney Studios', location: 'Burbank, CA')
-      studio_3 = Studio.create!(name: '20th Century Studios', location: 'Century City, CA')
+      studio_1 = Studio.create(name: 'Universal Pictures', location: 'Universal City, CA')
+      studio_2 = Studio.create(name: 'Walt Disney Studios', location: 'Burbank, CA')
+      studio_3 = Studio.create(name: '20th Century Studios', location: 'Century City, CA')
 
       visit studio_index_path
 
@@ -18,13 +18,13 @@ RSpec.describe 'the studio index page' do
     end
 
     it 'lists each studios movie titles' do
-      studio_1 = Studio.create!(name: 'Universal Pictures', location: 'Universal City, CA')
-      studio_2 = Studio.create!(name: 'Walt Disney Studios', location: 'Burbank, CA')
+      studio_1 = Studio.create(name: 'Universal Pictures', location: 'Universal City, CA')
+      studio_2 = Studio.create(name: 'Walt Disney Studios', location: 'Burbank, CA')
 
-      movie_1 = studio_1.movies.create!(title: 'The Birds', creation_year: 1963, genre: 'Horror')
-      movie_2 = studio_1.movies.create!(title: 'Jaws', creation_year: 1975, genre: 'Thriller')
-      movie_3 = studio_2.movies.create!(title: 'Pirates of the Caribbean: The Curse of the Black Pearl', creation_year: 2003, genre: 'Fantasy')
-      movie_4 = studio_2.movies.create!(title: 'Mary Poppins', creation_year: 1964, genre: 'Musical')
+      movie_1 = studio_1.movies.create(title: 'The Birds', creation_year: 1963, genre: 'Horror')
+      movie_2 = studio_1.movies.create(title: 'Jaws', creation_year: 1975, genre: 'Thriller')
+      movie_3 = studio_2.movies.create(title: 'Pirates of the Caribbean: The Curse of the Black Pearl', creation_year: 2003, genre: 'Fantasy')
+      movie_4 = studio_2.movies.create(title: 'Mary Poppins', creation_year: 1964, genre: 'Musical')
 
       visit studio_index_path
 
